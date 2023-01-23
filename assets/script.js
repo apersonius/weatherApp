dcfceb6a54557241b03f8a156f1b020ae5907daf
apersonius <personius12@gmail.com>
2023-01-23 12:26:25 -0600

    add API and create divs in HTML

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,28 @@
 //javascript
+let searchBtn=document.getElementById('searchBtn')
+let APIKEY = 'ef1f171918d3fb34ad0432afd2abf05f'
+
+function searchWeatherData(event) {
+    event.preventDefault();
+
+    let cityName = document.getElementById('cityName').value;
+    let url=`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKEY}`;
+
+    fetch(url).then((response)=>response.json()).then((data)=>{
+        console.log(data);
+
+     forecastData(cityName)   
+    })
+
+}
+
+function forecastData(cityName) {
+    let url=`api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${APIKEY}`;
+
+    fetch(url).then((response)=>response.json()).then((data)=>{
+        console.log(data);
+    })
+}
+
+
+searchBtn.addEventListener('click', searchWeatherData);
